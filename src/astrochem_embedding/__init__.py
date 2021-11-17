0af3e514e15d2c17fbf36fb6dece3ac77b2f1aec
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-11-16 19:13:11 -0500

    feat: exposing classes in main module

--- src/astrochem_embedding/__init__.py
+++ src/astrochem_embedding/__init__.py
@@ -2,3 +2,6 @@
 
 from astrochem_embedding.utils import get_paths, Translator
 from astrochem_embedding import models, pipeline, layers
+
+from astrochem_embedding.models.models import VICGAE, GRUAutoEncoder
+from astrochem_embedding.pipelines.data import MaskedStringDataModule, StringDataModule
