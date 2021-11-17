a9367e9b651b10ccf41132aceef05297f2b9141e
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-11-16 19:34:09 -0500

    modifications to train script for final training

--- scripts/train.py
+++ scripts/train.py
@@ -2,32 +2,28 @@
 import torch
 import pytorch_lightning as pl
 
-from astrochem_embedding.pipeline.data import SELFIESData
+from astrochem_embedding.pipeline.data import SELFIESData, StringDataModule, MaskedStringDataModule
 from astrochem_embedding import get_paths
 from astrochem_embedding.models import models
 
 pl.seed_everything(215015)
 
-BATCH_SIZE = 64
-NUM_WORKERS = 0
+BATCH_SIZE = 128
+NUM_WORKERS = 12
 EMBEDDING_DIM = 128
 Z_DIM = 32
 NUM_LAYERS = 1
 LR = 1e-4
 
-model = models.GRUAutoEncoder(EMBEDDING_DIM, Z_DIM, NUM_LAYERS, lr=LR)
+model = models.VICGAE(EMBEDDING_DIM, Z_DIM, NUM_LAYERS, lr=LR)
 
-data = SELFIESData(BATCH_SIZE, NUM_WORKERS)
+data = MaskedStringDataModule(BATCH_SIZE, NUM_WORKERS)
 
-logger = pl.loggers.TensorBoardLogger("tb_logs", name="AstrochemEmbedder")
+logger = pl.loggers.TensorBoardLogger("tb_logs", name="VICAstrochemEmbedder", log_graph=True)
 summarizer = pl.callbacks.ModelSummary(max_depth=-1)
 
-trainer = pl.Trainer(max_epochs=5, callbacks=[summarizer], gpus=1, logger=logger, precision=32)
-
-torch.cuda.empty_cache()
-
+trainer = pl.Trainer(max_epochs=5, callbacks=[summarizer], gpus=1, logger=logger)
 trainer.fit(model, data)
 
 paths = get_paths()
-
-trainer.save_checkpoint(paths.get("models").joinpath("GRUAutoEncoder.ckpt"))
+trainer.save_checkpoint(paths.get("models").joinpath("VICGAE.ckpt"))
