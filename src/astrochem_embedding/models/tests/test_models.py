15a49b3d22507b26707f1471cee4ab536c4d6736
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-11-16 20:03:21 -0500

    tests: added tests for models

---
+++ src/astrochem_embedding/models/tests/test_models.py
@@ -0,0 +1,52 @@
+
+import torch
+
+from astrochem_embedding.models import models
+
+
+@torch.no_grad()
+def test_gruautoencoder():
+    model = models.GRUAutoEncoder(embedding_dim=8, z_dim=16, num_layers=1)
+    # expect a tensor of shape batch, seq_length
+    test_array = torch.randint(0, 10, size=(32, 10))
+    output = model(test_array)
+
+
+@torch.no_grad()
+def test_gruautoencoder_multi():
+    model = models.GRUAutoEncoder(embedding_dim=8, z_dim=16, num_layers=3)
+    # expect a tensor of shape batch, seq_length
+    test_array = torch.randint(0, 10, size=(32, 10))
+    output = model(test_array)
+
+
+@torch.no_grad()
+def test_gruautoencoder_step():
+    model = models.GRUAutoEncoder(embedding_dim=8, z_dim=16, num_layers=1)
+    # expect a tensor of shape batch, seq_length
+    test_array = torch.randint(0, 10, size=(32, 10))
+    model.step((test_array, test_array, test_array), "test")
+
+
+@torch.no_grad()
+def test_vicgae():
+    model = models.VICGAE(embedding_dim=8, z_dim=16, num_layers=1)
+    # expect a tensor of shape batch, seq_length
+    test_array = torch.randint(0, 10, size=(32, 10))
+    output = model(test_array)
+
+
+@torch.no_grad()
+def test_vicgae_multi():
+    model = models.VICGAE(embedding_dim=8, z_dim=16, num_layers=3)
+    # expect a tensor of shape batch, seq_length
+    test_array = torch.randint(0, 10, size=(32, 10))
+    output = model(test_array)
+
+
+@torch.no_grad()
+def test_vicgae_step():
+    model = models.VICGAE(embedding_dim=8, z_dim=16, num_layers=1)
+    # expect a tensor of shape batch, seq_length
+    test_array = torch.randint(0, 10, size=(32, 10))
+    model.step((test_array, test_array, test_array), "test")
